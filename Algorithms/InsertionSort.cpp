fcaa526e89985f6cd611f9780eca6e070c23d9cc
Taha-A-Hashmi <taha.a.hashmi@gmail.com>
2024-09-30 00:34:11 +0500

    .

--- Algorithms/InsertionSort.cpp
+++ Algorithms/InsertionSort.cpp
@@ -4,8 +4,14 @@ using namespace std;
 
 
 void insertionSort(int arr[], int length) {
-    for (int i = 0; i < length; i++) {
-        
+    for (int i = 1; i < length; i++) {
+        int temp = arr[i];
+        int j = i - 1;
+        while (arr[j] > temp && j >= 0) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
     }
 }
 
